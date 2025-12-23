418b971a2e2d9c10ce2557f65c6eebfb662c198a
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-12-23 08:30:24 +0400

    testing issue with security?

--- tests/test_commands/test_materialize.py
+++ tests/test_commands/test_materialize.py
@@ -485,8 +485,13 @@ class TestInjectCommand:
 
         # Verify the git clone command used GitLab URL
         clone_call = mock_subprocess.call_args_list[0]
-        assert "gitlab.com" in str(clone_call)
-        assert "mygroup/myproject.git" in str(clone_call)
+        #print(clone_call)
+        #print(str(clone_call))
+        #assert False
+        assert "https://gitlab.com/mygroup/myproject.git" in str(clone_call)
+
+        #assert "gitlab.com" in str(clone_call)
+        #assert "mygroup/myproject.git" in str(clone_call)
 
     @patch("rhiza.commands.materialize.subprocess.run")
     @patch("rhiza.commands.materialize.shutil.rmtree")
@@ -529,8 +534,9 @@ class TestInjectCommand:
 
         # Verify the git clone command used GitHub URL
         clone_call = mock_subprocess.call_args_list[0]
-        assert "github.com" in str(clone_call)
-        assert "jebel-quant/rhiza.git" in str(clone_call)
+        print(clone_call)
+        assert "https://github.com/jebel-quant/rhiza.git" in str(clone_call)
+
 
     @patch("rhiza.commands.materialize.subprocess.run")
     @patch("rhiza.commands.materialize.shutil.rmtree")
